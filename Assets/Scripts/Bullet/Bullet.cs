3e262c871fe5b395f5bc07cc0d25fc8eb438a903
Łukasz Potaczek <lukasz.potaczek@fortedigital.no>
2023-01-09 19:00:04 +0100

    Add shooting

---
+++ Assets/Scripts/Bullet/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Bullet
+{
+    public class Bullet : MonoBehaviour
+    {
+        [SerializeField] private float speed = 10;
+        
+        private Rigidbody2D _rigidbody;
+        private Vector2 _direction;
+
+        public void SetDirection(Vector2 direction)
+        {
+            _direction = direction;
+        }
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody2D>();
+        }
+
+        private void Update()
+        {
+            _rigidbody.velocity = _direction.normalized * speed;
+        }
+    }
+}
---
+++ Assets/Scripts/Turret/TurretShooting.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Turret
+{
+    public class TurretShooting : MonoBehaviour
+    {
+        [SerializeField] private GameObject bulletPrefab;
+        [SerializeField] private float spawnBulletDuration;
+
+        private Camera _mainCamera;
+        private float _bulletTimer;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        }
+
+        void Update()
+        {
+            _bulletTimer -= Time.deltaTime;
+
+            if (_bulletTimer <= 0 && Touchscreen.current.primaryTouch.press.isPressed)
+            {
+                var touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+                Vector2 targetPosition = _mainCamera.ScreenToWorldPoint(touchPosition);
+
+                var spawnPosition = Vector2.MoveTowards(transform.position, targetPosition, .75f);
+                var direction = targetPosition - spawnPosition;
+
+                var bullet = Instantiate(bulletPrefab, spawnPosition, Quaternion.identity);
+                var bulletControls = bullet.GetComponent<Bullet.Bullet>();
+                bulletControls.SetDirection(direction);
+                _bulletTimer = spawnBulletDuration;
+            }
+        }
+    }
+}
